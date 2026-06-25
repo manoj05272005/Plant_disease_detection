ea29ba81b8776cdb9ed2cbf51fb6b0c9131146e0
manoj05272005 <kmanojvarun05@gmail.com>
2026-06-25 15:47:58 +0530

    Upload frontend/lib/services/api_config.dart

---
+++ frontend/lib/services/api_config.dart
@@ -0,0 +1,10 @@
+class ApiConfig {
+  // Base endpoints and keys for backend and weather API.
+  static const String baseHost =
+      'https://bond60-plant-disease-detection.hf.space';
+  static const String baseUrl = '$baseHost/api/v1';
+  static const String expectedPasskeyRpId =
+      'bond60-plant-disease-detection.hf.space';
+  static const String weatherApiBaseUrl = 'http://api.weatherapi.com/v1';
+  static const String weatherApiKey = 'cd3496d48c4442ed860142708260502';
+}
